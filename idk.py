0d0e2c761df2e416c3bf0a72d52e336429af057c
adlbs03 <adrienlebois@hotmail.com>
2025-11-25 18:50:39 -0500

    change the name

--- idk.py
+++
@@ -1,9 +0,0 @@
-
-while True:
-    user_password = input("Enter a password: ")
-
-    if len(user_password) < 6 or user_password.isdigit():
-        user_password
-    else:
-        print("Your password is good")
-        break
